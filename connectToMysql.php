fcf6cfd851444bd89ce13642a46988143182e2dd
ManhCoder1201 <98036877+ManhCoder1201@users.noreply.github.com>
2022-04-16 01:42:50 +0700

    Update connectToMysql.php

--- connectToMysql.php
+++ connectToMysql.php
@@ -1,10 +1,10 @@
 <?php
-$GLOBALS['conn'] = mysqli_connect("localhost", "root", "", "gadgetstore");
+$GLOBALS['conn'] = mysqli_connect("3.132.234.157", "manh48", "123@123", "gadgetstore");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$connecti=mysqli_connect("localhost", "root", "", "gadgetstore");
-$link = mysqli_connect("localhost", "root", "", "gadgetstore");
+$connecti=mysqli_connect("3.132.234.157", "manh48", "123@123", "gadgetstore");
+$link = mysqli_connect("3.132.234.157", "manh48", "123@123", "gadgetstore");
 ?>
 
